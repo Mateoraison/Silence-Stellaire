f3ca76b80c3886339ba8b70972e162067e1d7691
joshuadonne <joshuadonne761@gmail.com>
2026-02-11 16:11:23 +0100

    Mise a jour

--- src/jeux.c
+++ src/jeux.c
@@ -90,7 +90,6 @@ void charger_tilemap(SDL_Renderer *renderer, SDL_Texture *tileset,
             SDL_FRect src;
             float type_x = 0;
             float type_y = 0;
-            float type_x = 0;
 
             switch(map[x][y].type){
                 case terreP:
