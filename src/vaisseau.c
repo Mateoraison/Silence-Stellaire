9346c8868aaf50e78663d3405039e67939ec4a8c
Maxime Leroux <maximeleroux99@gmail.com>
2026-03-03 14:45:46 +0100

    Changement de tileset pour le vaisseau, modification de la pge html pour générer un txt pour la map du vaisseau et mise à jour du vaisseau.c

--- src/vaisseau.c
+++ src/vaisseau.c
@@ -3,157 +3,211 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-
-#define TILE_SIZE 32
-#define TILESET_TILE_SIZE 32
-#define TILESET_COLUMNS 10 // Ton image a 10 tuiles par ligne
+#define TILE_SIZE 64
 #define MAP_W 24
 #define MAP_H 19
 
-/*
----- Sols ----
-0 Sol métal clar 
-1 Sol métal sombre 
-2 Sol avec lignes 
-3 Sol grillagé 
+int tile_map[MAP_H][MAP_W];
 
----- Murs ----
-10 Mur plein 
-11 Mur avec panneaux 
-12 Mur avec tuyaux 
-13 Mur avec lumière 
-14 Mur coin gauche 
-15 Mur coin droit 
+typedef struct {
+    int tileX;
+    int tileY;
+} TileRef;
 
----- Structures ----
-20 Porte fermée 
-21 Porte ouverte 
-22 Sas 
-23 Hublot 
-24 Fenêtre spatiale 
+void charger_map(const char* filename) {
+    // 1. Initialisation forcée à -1
+    for (int y = 0; y < MAP_H; y++) {
+        for (int x = 0; x < MAP_W; x++) tile_map[y][x] = -1;
+    }
 
----- Consoles & déco ----
-30 Console éteinte 
-31 Console allumée 
-32 Ecran radar 
-33 Terminal
-34 Panneau d'alerte 
-
----- Détails techniques ----
-40 Grille d'aération 
-41 Câbles 
-42 Générateur 
-43 Réacteur 
-44 Batterie 
-*/
-
-int tile_map[MAP_H][MAP_W]; 
-
-void remplir_tileset2(int tile_map[MAP_H][MAP_W]){
-
-    FILE *file = fopen("assets/map/vaisseau.txt", "r");
+    FILE *file = fopen(filename, "r");
     if (!file) {
-        SDL_Log("Erreur ouverture fichier Map : %s", SDL_GetError());
+        SDL_Log("ERREUR : Impossible d'ouvrir %s", filename);
         return;
     }
 
+    int count = 0;
     for (int y = 0; y < MAP_H; y++) {
         for (int x = 0; x < MAP_W; x++) {
-            fscanf(file, "%d-", &tile_map[y][x]);
-            printf("Type de la tuile [%d][%d] : %d\n", x, y, tile_map[y][x]);
-
+            // " %d-" avec l'espace au début permet d'ignorer les retours à la ligne
+            if (fscanf(file, " %d-", &tile_map[y][x]) == 1) {
+                count++;
+            }
         }
-        fscanf(file, "\n"); // Lire le saut de ligne à la fin de chaque ligne de la carte
     }
-
     fclose(file);
+    SDL_Log("Map chargee : %d tuiles lues.", count);
 }
 
+/*
+Comment trouver les bonnes coordonnées X, Y ?
+
+Ouvre ton fichier tileset_sf.png dans un logiciel de dessin (Paint, Photoshop, GIMP) :
+S'aider de piskelapp.com pour trouver les bonnes coordonnées X, Y de chaque tuile dans le tileset.
+
+Place ton curseur sur la tuile voulue.
+
+Regarde les coordonnées en pixels (ex: x=384, y=320).
+
+Divise par 64 (ex: 384/64 = 12, 320/64 = 10).
+
+Tes coordonnées sont {12, 10} dans la palette.
+*/
 
 void draw_map(SDL_Renderer *renderer, SDL_Texture *tileset) {
+    /*
+    Non obligatoire, mais si je veux faire du scaling pour que ça prenne toute la fenêtre :
+    SDL_Rect fenetre ;
+    SDL_GetRenderViewport(renderer, &fenetre);
+    float scaleX = (float)fenetre.w / (MAP_W * TILE_SIZE);
+    float scaleY = (float)fenetre.h / (MAP_H * TILE_SIZE);
+    */
+
+    // Les coordonnées (X, Y) correspondent au nombre de cases de 64px 
+    // en partant du haut à gauche du PNG.
+    TileRef palette[] = {
+
+        // Coutours & Extérieur 
+        {10, 0},    // [0] Tuile vide
+        {19, 6},   // [1] Contour coin haut gauche 
+        {21, 6},   // [2] Contour coin haut droit 
+        {19, 8},   // [3] Contour coin bas gauche 
+        {21, 8},   // [4] Contour coin bas droit 
+        {21, 7},   // [5] Contour vertical 
+        {20, 6},  // [6] Contour horizontal haut
+        {20, 8},  // [7] Contour horizontal bas
+        {16, 8}, // [8] Bout de couloir horizontal gauche 
+        {18, 8}, // [9] Bout de couloir horizontal droit 
+        {8, 6}, // [10] Premier bout du réacteur 
+        {9, 6}, // [11] Deucième bout du réacteur 
+
+
+        // Sols
+        {3, 12},  // [12] Sol simple 
+        {5, 10}, // [13] Sol avec grille 
+        {5, 11}, // [14] Sol avec ventilation 
+        {3, 10}, // [15] Sol avec Motif 1
+        {4,10}, // [16] Sol avec Motif 2
+        {3, 11}, // [17] Sol avec Motif 3 
+        {4, 11}, // [18] Sol avec Motif 4 
+
+
+        // Murs avec Sols pour omnbre 
+        {10, 9}, // [19] Mur Simple 
+        {10, 10}, // [20] Sol avec ombre 
+
+        {13, 6}, // [21] Mur avec LED Verte 
+        {13, 7}, // [22] Mur avec LED Bleu 
+        {13, 8}, // [23] Mur avec LED Jaune 
+        {13, 9}, // [24] Mur avec LED Rouge 
+        {13,10}, // [25] Sol avec ombre 
+
+        {11, 9}, // [26] Mur avec Fenêtre 
+        {11, 10}, // [27] Sol avec ombre 
+
+        {12,9}, // [28] Mur avec Poteau 
+        {12,10}, // [29] Sol avec ombre 
+
+        {14, 9}, // [30] Mur avec chiffre 
+        {14,10}, // [31] Sol avec ombre 
+
+        {15, 9}, // [32] Mur avec Soin 
+        {15, 10}, // [33] Sol avec ombre 
+
+        {16, 9}, // [34] Mur O3
+        {16, 10}, // [35] Sol avec ombre 
+
+        {17, 9}, // [36] Mur Ouvert sombre 
+        {17, 10}, // [37] Sol avec ombre 
+
+        {18, 9}, // [38] Mur Ouvert 
+        {18, 10}, // [39] Sol avec ombre 
+
+        {19, 9}, // [40] Mur O2
+        {19, 10}, // [41] Sol avec ombre 
+
+        {20, 9}, // [42] Mur avec Cable 
+        {20, 10}, // [43] Sol avec Ombre 
+
+        {22, 9}, // [44] Mur avec Aération 
+        {22, 10}, // [45] Sol avec ombre 
+
+
+        // Accessoires 
+        {9, 16}, // [46] Premier bout de l'écran 
+        {10, 16}, // [47] Deucième bout de l'écran 
+
+        {15, 5}, // [48] Flèche vers la droite 
+        {15, 6}, // [49] Flèche vers le bas 
+        {15, 7}, // [50] Flèche vers le haut 
+        {15, 8}, // [51] Flèche vers la gauche 
+
+        {0, 0}, // [52] Caméra 1
+        {0, 1}, // [53] Caméra 2
+        {1, 0}, // [54] Caméra 3
+
+    };
+
     for (int y = 0; y < MAP_H; y++) {
         for (int x = 0; x < MAP_W; x++) {
-            int tile_id = tile_map[y][x];
+            int type = tile_map[y][x];
 
-            // Calcul de la position dans le tileset (grille 10x10)
-            // Ligne = ID / Colonnes, Colonne = ID % Colonnes
+            // Si type est -1, on ne dessine rien
+            if (type < 0) continue; 
+            
+            // Sécurité pour ne pas dépasser la taille de la palette 
+            if (type >= sizeof(palette)/sizeof(palette[0])) type = 0;
 
-            SDL_FRect srcRect = {
-                (float)(tile_id % TILESET_COLUMNS) * TILESET_TILE_SIZE,
-                (float)(tile_id / TILESET_COLUMNS) * TILESET_TILE_SIZE,
-                TILESET_TILE_SIZE,
-                TILESET_TILE_SIZE
+            SDL_FRect src = { 
+                (float)palette[type].tileX * TILE_SIZE, 
+                (float)palette[type].tileY * TILE_SIZE, 
+                TILE_SIZE, TILE_SIZE 
             };
-            SDL_FRect dstRect = { (float)x * TILE_SIZE, (float)y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
-            SDL_RenderTexture(renderer, tileset, &srcRect, &dstRect);
+            
+            SDL_FRect dst = { 
+                (x * TILE_SIZE / 2 ), 
+                (y * TILE_SIZE / 2 ), 
+                TILE_SIZE / 2, TILE_SIZE / 2
+            };
+            
+            SDL_RenderTexture(renderer, tileset, &src, &dst);
         }
     }
-}
-
-bool is_blocking(int tile) {
-    return (
-        ((tile >= 10) && (tile <= 19)) ||   // murs
-        tile == 20 ||                 // porte fermée
-        tile == 22                    // sas
-    );
 }
 
 int vaisseau(SDL_Renderer *renderer) {
+    SDL_Texture *t_tiles = IMG_LoadTexture(renderer, "assets/tileset/V2/Interieur_Vaisseau/tileset_sf.png");
+    SDL_Texture *t_bg = IMG_LoadTexture(renderer, "assets/tileset/V2/Interieur_Vaisseau/Grafika/bg.png"); 
 
-    SDL_Texture *tileset =
-        IMG_LoadTexture(renderer,
-        "assets/tileset/V2/Interieur_Vaisseau/free_tiles.png");
-
-    if (!tileset) {
-        SDL_Log("Erreur: %s", SDL_GetError());
-        return 1;
+    if (!t_tiles) {
+        SDL_Log("Erreur Texture Tileset: %s", SDL_GetError());
+        return -1;
     }
 
-    SDL_SetTextureScaleMode(tileset, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(t_tiles, SDL_SCALEMODE_NEAREST);
+    charger_map("assets/map/vaisseau.txt");
 
-    remplir_tileset2(tile_map);
-
-    SDL_FRect player = {400, 300, 20, 20};
     bool running = true;
     SDL_Event event;
 
     while (running) {
-
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT)
-                return 0; // quitter le jeu
+            if (event.type == SDL_EVENT_QUIT) running = false;
         }
 
-        const bool *state = SDL_GetKeyboardState(NULL);
-
-        float next_x = player.x;
-        float next_y = player.y;
-
-        if (state[SDL_SCANCODE_W]) next_y -= 3;
-        if (state[SDL_SCANCODE_S]) next_y += 3;
-        if (state[SDL_SCANCODE_A]) next_x -= 3;
-        if (state[SDL_SCANCODE_D]) next_x += 3;
-
-        int grid_x = (int)(next_x + 10) / TILE_SIZE;
-        int grid_y = (int)(next_y + 10) / TILE_SIZE;
-
-        if (!is_blocking(tile_map[grid_y][grid_x])) {
-            player.x = next_x;
-            player.y = next_y;
-        }
-
-        SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
         SDL_RenderClear(renderer);
 
-        draw_map(renderer, tileset);
+        if (t_bg) {
+            SDL_RenderTexture(renderer, t_bg, NULL, NULL);
+        }
 
-        SDL_SetRenderDrawColor(renderer,0,255,255,255);
-        SDL_RenderFillRect(renderer,&player);
+        draw_map(renderer, t_tiles);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
     }
 
-    SDL_DestroyTexture(tileset);
-    return 1;
+    SDL_DestroyTexture(t_tiles);
+    SDL_DestroyTexture(t_bg);
+    return 0;
 }
